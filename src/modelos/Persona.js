b8cea12c3c2a7b7d4b172cba74c36d88dcafad88
Xavier <xagp_07@hotmail.com>
2023-05-21 12:33:13 -0500

    subida de proyecto

--- src/modelos/Persona.js
+++ src/modelos/Persona.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 //mongoose.connect("mongodb://mongo/SistemaSoporte");
-mongoose.connect("mongodb://sistemasoportebd:fvZSK20jZY3qyWiikSmY9dRQwaWfXTdM51nVimiv4RRz7IyXSRoqtyPDj6Uv2W4RARO7YzTpNKvW7d7DJpZFrg==@sistemasoportebd.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@sistemasoportebd@");
+mongoose.connect('mongodb+srv://Xavier07:Xg1105531949@lck.mevmnhk.mongodb.net/?retryWrites=true&w=majority');
 
 
 var personaSchema = new Schema({
